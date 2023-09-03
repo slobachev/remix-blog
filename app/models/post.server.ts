c0001e6aed057246c36c9b44439119d15285cd5b
Sergey Lobachev <sergeylobachev@outlook.com>
2023-09-03 15:07:48 +0300

    rc/1.0.0 (#1)
    * add some APIs and amin page
    
    * add progressive enhancement; fix links
    
    * update readme

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,17 +1,15 @@
-type Post = {
-  slug: string;
-  title: string;
-};
+import type { Post } from "@prisma/client";
+import { prisma } from "~/db.server";
 
-export async function getPosts(): Promise<Array<Post>> {
-  return [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
+
+export async function getPosts() {
+  return prisma.post.findMany();
+}
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug }});
+}
+
+export async function createPost(post: Pick<Post, 'slug' | 'title' | 'markdown'>) {
+  return prisma.post.create({ data: post });
 }
